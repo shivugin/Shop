785659cb71fa6062a37dc5b6cb5581d775343c58
Shiva <shivugin@hotmail.com>
2016-10-16 17:27:12 +0100

    Method to get the prices of the fruits

--- src/main/scala/com/shop/ShoppingCart.scala
+++ src/main/scala/com/shop/ShoppingCart.scala
@@ -1,8 +1,21 @@
 package com.shop
 
+import scala.math.BigDecimal.RoundingMode
+
 /**
   * Created by Shiva on 16/10/2016.
   */
 object ShoppingCart {
+
+  // Each Item price.
+  private val itemsPriceList: Map[String, Double] = Map(("apple", 0.60), ("orange", 0.25))
+
+  def getPrice(item: String): Double = {
+    itemsPriceList.getOrElse(item toLowerCase, 0)
+  }
+
+  def price(item: String): Option[Double] = itemsPriceList.get(item toLowerCase)
+
+
 
 }
